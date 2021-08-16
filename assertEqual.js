2e86d3c202e0c27f77639a22c2852e1ed345e16f
denizedaza <dazadenize@gmail.com>
2021-08-16 19:15:37 -0400

    Added first assertEqual file

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}✅✅`);
+  } else {
+    console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
