0f5227a3e6a4edde12ad2218ba3c0bb3a57811c2
denizedaza <dazadenize@gmail.com>
2021-08-20 20:48:24 -0400

    added recursive capabilites

--- eqObjects.js
+++ eqObjects.js
@@ -7,11 +7,11 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   let result = false;
 
   if (array1.length === array2.length) {
-    
+
     if (array1.every((v, i) => v === array2[i])) {
       result = true;
     }
@@ -25,29 +25,25 @@ const eqObjects = function (object1, object2) {
   const obj1Keys = Object.keys(object1);
   const obj2Keys = Object.keys(object2);
 
-  let result = false;
-
   if (obj1Keys.length !== obj2Keys.length) {
-    console.log("Objects aren't the same length. They're not equal. False")
-    result = false;
-    //process.exit();
-  } else {
-    for (let obj1 of obj1Keys) {
-      console.log("obj1:", obj1);
-      console.log("obj1 value:", object1[obj1]);
-      console.log("obj2 value:", object2[obj1]);
-      if (Array.isArray(object1[obj1]) && Array.isArray(object2[obj1])){
-        eqArrays(object1[obj1], object2[obj1]);
-      } else if (object1[obj1] !== object2[obj1]) {
-        console.log("The key/value pairs don't match. Therefore, false");
-        result = false;
+    return false;
+  }
+  for (const key of obj1Keys) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
       }
     }
-    console.log("Objects are identical: true");
-    result = true;
   }
-  // console.log(typeof result);
-  return result;
+  return true;
 };
 
 const ab = { a: "1", b: "2" };
@@ -63,3 +59,9 @@ eqObjects(cd, dc); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 eqObjects(cd, cd2); // => false
+
+// new test code from recursion 
+eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }) // => true
+
+eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }) // => false
+eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }) // => false
