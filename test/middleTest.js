cfb318d331d0c255500d28cd2d2702695ba90f87
denizedaza <dazadenize@gmail.com>
2021-08-23 14:05:15 -0400

    changed test cases for middle and tail test files for refactoring with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,23 +1,36 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-// For arrays with one or two elements, there is no middle. Return an empty array.
-middle([1]); // => []
-middle([1, 2]); // => []
-// For arrays with odd number of elements, an array containing a single middle element should be returned.
+// // For arrays with one or two elements, there is no middle. Return an empty array.
+// middle([1]); // => []
+// middle([1, 2]); // => []
+// // For arrays with odd number of elements, an array containing a single middle element should be returned.
 
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
+// middle([1, 2, 3]); // => [2]
+// middle([1, 2, 3, 4, 5]); // => [3]
 
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+// // For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+// middle([1, 2, 3, 4]); // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle", () => {
+  it("returns an empty array [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns an empty array [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns an array with a single middle element for arrays with an odd number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns an array with a single middle element for arrays with an odd number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns an array with two middle elements for arrays with an even number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns an array with two middle elements for arrays with an even number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,28 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+describe("#tail", () => {
+  it("returns 3 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  // it("returns '5' for ['5']", () => {
+  //   assert.strictEqual(head(['5']), '5'); 
+  // });
+  // it("returns '5' for ['5, 6, 7']", () => {
+  //   assert.strictEqual(head(['5', '6', '7']), '5'); 
+  // });
+  // it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+  //   assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello'); 
+  // });
+  // it("returns 'One' for ['One']", () => {
+  //   assert.strictEqual(head(['One']), 'One'); 
+  // });
+  // it("returns 'undefined' for [ ]", () => {
+  //   assert.strictEqual(head([ ]), undefined); 
+  // });
+});
