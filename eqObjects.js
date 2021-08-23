524ab6cf7878cf5a39d0979e740f2696bdcde61b
denizedaza <dazadenize@gmail.com>
2021-08-23 12:48:47 -0400

    refactored assertEqual and head files

--- eqObjects.js
+++ eqObjects.js
@@ -46,6 +46,8 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }) );
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 // eqObjects(ab, ba); // => true
--- head.js
+++ head.js
@@ -1,17 +1,5 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}✅✅`);
-  } else {
-    console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑`);
-  }
-};
-
 const head = function(array){
   return array[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["One"]), "One");
-assertEqual(head([ ]), undefined);
+module.exports = head;
