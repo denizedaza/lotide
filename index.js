823befa3a343869c56f5e02f1afbe5bf6785025f
denizedaza <dazadenize@gmail.com>
2021-08-23 14:43:21 -0400

    made changes to index file for lotide object

---
+++ index.js
@@ -0,0 +1,18 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+
+};
